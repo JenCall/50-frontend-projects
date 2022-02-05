c2d3b56680c19f7ffd739ff6b839beeb92ddf1e3
Jeniffer Callou <jenifferprc@gmail.com>
2022-02-05 11:02:27 +0100

    adjust js

--- 10-dad-jokes/script.js
+++ 10-dad-jokes/script.js
@@ -1,2 +1,19 @@
-const joke = document.getElementById('joke');
+const jokeEl = document.getElementById('joke');
 const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+function generateJoke() {
+    const config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
+    fetch('https://icanhazdadjoke.com', config)
+    .then(res => res.json())
+    .then((data) => {
+        jokeEl.innerText = data.joke
+    })
+}
