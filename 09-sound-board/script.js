f8d2df6df7c9fd15e3c036f5d7b558a53a3c822b
Jeniffer Callou <jenifferprc@gmail.com>
2022-02-05 09:45:54 +0100

    add sounds + js

---
+++ 09-sound-board/script.js
@@ -0,0 +1,10 @@
+const sounds = ['applause', 'boo', 'grasp', 'tada', 'victory', 'wrong']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    document.getElementById('buttons').appendChild(btn)
+})
