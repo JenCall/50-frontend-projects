b902b589adfbd8360e3a8b51b0215b7abc4a965f
Jeniffer Callou <jenifferprc@gmail.com>
2022-02-06 21:34:38 +0100

    add part of html in js

--- 11-event-keycodes/script.js
+++ 11-event-keycodes/script.js
@@ -1,5 +1,18 @@
-const insert = document.getElementById('insert')
+const insert = document.getElementById('insert');
 
 window.addEventListener('keydown', (e) => {
-    console.log(e)
+    insert.innerHTML = `
+    <div class="key">
+        ${e.key} 
+        <small>event.key</small>
+    </div>
+    <div class="key">
+        65 
+        <small>event.keyCode</small>
+    </div>
+    <div class="key">
+        KeyA 
+        <small>event.code</small>
+    </div>
+    `
 })
