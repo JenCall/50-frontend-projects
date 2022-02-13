66594ef60e2760e89b12bffbf98959cda4390c75
Jeniffer Callou <jenifferprc@gmail.com>
2022-02-13 18:28:48 +0100

    add js dark+light mode function

--- 19-theme-clock/script.js
+++ 19-theme-clock/script.js
@@ -4,3 +4,18 @@ const secondEl = document.querySelector('.second');
 const timeEl = document.querySelector('.time');
 const dateEl = document.querySelector('.date');
 const toggleEl = document.querySelector('.toggle');
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+
+toggleEl.addEventListener('click', (event) => {
+    const html = document.querySelector('html')
+    if(html.classList.contains('dark')) {
+        html.classList.remove('dark')
+        event.target.innerHTML = 'Dark mode'
+    } else {
+        html.classList.add('dark')
+        event.target.innerHTML = 'Light mode'
+    }
+})
