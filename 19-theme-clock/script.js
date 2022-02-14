3c9623bd92dc908277e746a7f09d23559e612290
Jeniffer Callou <jenifferprc@gmail.com>
2022-02-14 20:08:59 +0100

    add scale min + seconds

--- 19-theme-clock/script.js
+++ 19-theme-clock/script.js
@@ -31,6 +31,12 @@ function setTime() {
 
     hourEl.style.transform = `translate(-50%, -100%) rotate(
         ${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+    
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(
+        ${scale(minutes, 0, 59, 0, 360)}deg)`    
+
+    secondEl.style.transform = `translate(-50%, -100%) rotate(
+        ${scale(seconds, 0, 59, 0, 360)}deg)`        
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
