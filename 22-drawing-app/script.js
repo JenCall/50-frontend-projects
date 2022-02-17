a00f3a1fdf24f01b3bedde812a153accd742ca18
Jeniffer Callou <jenifferprc@gmail.com>
2022-02-17 21:19:59 +0100

    add js file + drawing function

---
+++ 22-drawing-app/script.js
@@ -0,0 +1,30 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEL = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
+
+let size = 20
+let color = 'black'
+
+function drawCircle(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0, Math.PI * 2)
+    ctx.fillStyle = color
+    ctx.fill()
+}
+
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath()
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
+    ctx.strokeStyle = color
+    ctx.lineWidth = size * 2
+    ctx.stroke()
+}
+
+drawCircle(80, 160)
+drawLine(200, 200)
