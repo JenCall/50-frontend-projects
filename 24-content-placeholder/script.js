699833af8bed8b579c3add4951840e1b42a12a13
Jeniffer Callou <jenifferprc@gmail.com>
2022-02-19 12:10:26 +0100

    corrected placeholders + ids

--- 24-content-placeholder/script.js
+++ 24-content-placeholder/script.js
@@ -1,8 +1,8 @@
 const header = document.getElementById('header');
 const title = document.getElementById('title');
 const excerpt = document.getElementById('excerpt');
-const profile_img = document.getElementById('profile-img');
-const name = document.getElementById('name');
+const profile_img = document.getElementById('profile_img');
+const author_name = document.getElementById('name');
 const date = document.getElementById('date');
 
 const animated_bgs = document.querySelectorAll('.animated-bg');
@@ -14,8 +14,8 @@ function getData() {
     header.innerHTML = '<img src="https://images.unsplash.com/photo-1606818855637-f637cdb124b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" alt="dog picture">'
     title.innerHTML = 'Lorem ipsum dolor sit amet'
     excerpt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, vel.'
-    profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="author picture">';
-    name.innerHTML = 'John Doe'
+    profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="author picture">'
+    author_name.innerHTML = 'John Doe'
     date.innerHTML = 'Oct 08, 2022'
 
     animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'));
