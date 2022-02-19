c761b01d3b9424c2c55630e468d9a1dc7873563c
Jeniffer Callou <jenifferprc@gmail.com>
2022-02-19 11:55:13 +0100

    addimg getData function in js

--- 24-content-placeholder/script.js
+++ 24-content-placeholder/script.js
@@ -8,6 +8,17 @@ const date = document.getElementById('date');
 const animated_bgs = document.querySelectorAll('.animated-bg');
 const animated_bg_texts = document.getElementById('.animated-bg-text');
 
+setTimeout(getData, 2500)
+
 function getData() {
-    header.innerHTML = ''
+    header.innerHTML = '<img src="https://images.unsplash.com/photo-1606818855637-f637cdb124b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" alt="dog picture">'
+    title.innerHTML = 'Lorem ipsum dolor sit amet'
+    excerpt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, vel.'
+    profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="">';
+    name.innerHTML = 'John Doe'
+    date.innerHTML = 'Oct 08, 2022'
+
+    animated_bgs.forEach(bg => bg.classList.remove('animated-bg'));
+
+    animated_bg_texts.forEach(bg => bg.classList.remove('animated-bg-text'));
 }
