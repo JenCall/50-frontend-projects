13ade760d3ef5ea609bac0e0fc1a9d40a6199e0e
Jeniffer Callou <jenifferprc@gmail.com>
2022-03-06 11:52:59 +0100

    adjusted replay id

--- 34-animated-count-down/script.js
+++ 34-animated-count-down/script.js
@@ -1,7 +1,7 @@
 const nums = document.querySelectorAll('.nums span')
 const counter = document.querySelector('.counter')
 const finalMessage = document.querySelector('.final')
-const replay = document.querySelector('#replay')
+const replay = document.getElementById('replay')
 
 runAnimation()
 
