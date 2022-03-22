e662dd326827a4e557782d8442bcd91bda70f4a6
Jeniffer Callou <jenifferprc@gmail.com>
2022-03-22 21:00:59 +0100

    add variables form event listener

--- 49-todo-list/script.js
+++ 49-todo-list/script.js
@@ -1,3 +1,9 @@
 const form = document.getElementById('form')
 const input = document.getElementById('input')
 const todosUl = document.getElementById('todos')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    addTodo()
+})
