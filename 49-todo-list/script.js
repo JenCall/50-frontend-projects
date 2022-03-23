fc14b97a7b16998b23a08c52a2595b3b4798e906
Jeniffer Callou <jenifferprc@gmail.com>
2022-03-23 20:19:00 +0100

    add addTodo function + if condition for todos

--- 49-todo-list/script.js
+++ 49-todo-list/script.js
@@ -2,6 +2,12 @@ const form = document.getElementById('form')
 const input = document.getElementById('input')
 const todosUl = document.getElementById('todos')
 
+const todos = JSON.parse(localStorage.getItem('todos'))
+
+if(todos) {
+    todos.forEach(todo => addTodo(todo))
+}
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
@@ -9,7 +15,7 @@ form.addEventListener('submit', (e) => {
 })
 
 function addTodo(todo) {
-    let todoText = input.value;
+    let todoText = input.value
 
     if(todo) {
         todoText = todo.text
@@ -23,10 +29,22 @@ function addTodo(todo) {
 
         todoEl.innerText = todoText
 
-        todoEl.addEventListener('click', () => todoEl.classList.toggle('completed'))
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('completed')
+            updateLS()
+        }) 
+
+        todoEl.addEventListener('contextmenu', (e) => {
+            e.preventDefault()
+
+            todoEl.remove()
+            updateLS()
+        }) 
 
         todosUl.appendChild(todoEl)
 
         input.value = ''
+
+        updateLS()
     }
 }
