1f78c340d987b70c36409a3d4807bf7d721fdb22
Jeniffer Callou <jenifferprc@gmail.com>
2022-03-24 11:37:24 +0100

    add loadQuiz function- add answers

--- 46-quiz-app/script.js
+++ 46-quiz-app/script.js
@@ -37,7 +37,7 @@ const quiz = document.getElementById('quiz');
 const answerEls = document.querySelectorAll('.answer');
 const questionEl = document.getElementById('question');
 const a_text = document.getElementById('a_text');
-const b_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
 const submitBtn = document.getElementById('submit');
@@ -51,4 +51,8 @@ function loadQuiz() {
     const loadQuizData = quizData[currentQuiz]
 
     questionEl.innerText = loadQuizData.question
+    a_text.innerText = loadQuizData.a
+    b_text.innerText = loadQuizData.b
+    c_text.innerText = loadQuizData.c
+    d_text.innerText = loadQuizData.d
 }
