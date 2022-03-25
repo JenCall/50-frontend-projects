52713d0871dbee657c1f703f6c6154de61c49d22
Jeniffer Callou <jenifferprc@gmail.com>
2022-03-25 14:09:26 +0100

    add variables

---
+++ 45-netflix-navigation/script.js
@@ -0,0 +1,7 @@
+const openBtn = document.querySelector('.open-btn');
+const closeBtn = document.querySelector('.close-btn');
+const nav = document.querySelectorAll('.nav')
+
+// closeBtn.addEventListener('click', () => {
+//     classList.add('visible')
+// })
