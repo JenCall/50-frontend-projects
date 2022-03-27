41fbdf8f451bf623fdfcb4f15d781728cbbbada5
Jeniffer Callou <jenifferprc@gmail.com>
2022-03-27 21:47:03 +0200

    add variables

---
+++ 47-testimonial-box/script.js
@@ -0,0 +1,15 @@
+const testimonialContainer = document.querySelector('.testimonial-container');
+const imgSrc = document.querySelector('.user-image');
+const username = document.querySelector('.username');
+const role = document.querySelector('.role');
+const testimonial = document.querySelector('.testimonial');
+
+let index = 0
+
+// uploadInfo()
+
+// function uploadInfo() {
+//     const testimonialData = dataArray[idx];
+
+//     testimonial.innerText = testimonialData.testimonial
+// }
