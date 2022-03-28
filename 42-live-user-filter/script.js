34a82bb7d1b17ef687b2879cc889e67c3b66c64a
Jeniffer Callou <jenifferprc@gmail.com>
2022-03-28 11:01:29 +0200

    add filterData function

--- 42-live-user-filter/script.js
+++ 42-live-user-filter/script.js
@@ -32,3 +32,13 @@ async function uploadData() {
     })
 
 }
+
+function filterData(searchTerm) {
+    listItems.forEach(item => {
+        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+            item.classList.remove('hide')
+        } else {
+            item.classList.add('hide')
+        }
+    })
+}
