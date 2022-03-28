9ae34b10f5c00e568ead97993ec04647e58553f1
Jeniffer Callou <jenifferprc@gmail.com>
2022-03-28 10:46:45 +0200

    add uploadData function

--- 42-live-user-filter/script.js
+++ 42-live-user-filter/script.js
@@ -1,3 +1,29 @@
 const result = document.getElementById('result')
 const filter = document.getElementById('filter')
 const listItems = []
+uploadData()
+async function uploadData() {
+    const response = await fetch('https://randomuser.me/api?results=50');
+
+    const { results } = await response.json()
+
+    // clear results first
+    results.innerHTML = ''
+
+    results.forEach(user => {
+        const li = document.createElement('li')
+
+        listItems.push(li)
+
+        li.innerHTML = `
+            <img src="${user.picture.large}" alt="${user.name.first}">
+            <div class="user-info">
+                <h4>${user.name.first} ${user.name.last}</h4>
+                <p>${user.location.city}, ${user.location.country}</p>
+            </div>
+        `
+
+        result.appendChild(li)
+    })
+
+}
