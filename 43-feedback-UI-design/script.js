63f5c03517691d5f6c5581eeb3c80dc2123be399
Jeniffer Callou <jenifferprc@gmail.com>
2022-03-29 13:54:56 +0200

    add sendButton event + variable adjustments

--- 43-feedback-UI-design/script.js
+++ 43-feedback-UI-design/script.js
@@ -1,13 +1,26 @@
 const ratings = document.querySelectorAll('.rating');
+const ratingsContainer = document.querySelector('.rating-container');
 const sendBtn = document.getElementById('send');
 const panel = document.getElementById('panel');
+let selectedRating;
 
-// event bubling
-panel.addEventListener('click', (e) => {
+// event bubling / event propagation
+ratingsContainer.addEventListener('click', (e) => {
     if(e.target.parentNode.classList.contains('rating')) {
         removeActive()
         e.target.parentNode.classList.add('active')
+        selectedRating = e.target.nextElementSibling.innerHTML
     }
+})
+
+sendBtn.addEventListener('click', () => {
+    panel.innerHTML = `
+        <i class="fas fa-heart"></i>
+        <strong>Thank You!</strong>
+        <br>
+        <strong>Feedback: ${selectedRating}</strong>
+        <p>We use your feedback to improve our customer support.</p>
+    `
 })
 
 function removeActive() {
